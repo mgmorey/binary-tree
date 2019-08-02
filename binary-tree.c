2a2cf4bbe0468477ae58c2af8822053e377461b7
Michael G. Morey <mgmorey@gmail.com>
2019-08-01 18:41:09 -0700

    Add copyright text to source files.

--- binary-tree.c
+++ binary-tree.c
@@ -1,14 +1,26 @@
-/* binary-tree.c
+/* binary-tree.c */
+/* Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com> */
 
-     A
-    / \
-   /   \
-  B     E
- / \   / \
-|   | |   |
-C   D F   G
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
 
-*/
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+/*      A      */
+/*     / \     */
+/*    /   \    */
+/*   B     E   */
+/*  / \   / \  */
+/* |   | |   | */
+/* C   D F   G */
 
 #include <stdio.h>
 #include <stdlib.h>
